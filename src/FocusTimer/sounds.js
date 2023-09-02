212c51db44e128232c4dba1dc04ba1c97cc504c8
Bruno S Velho <bruno.velho@gmail.com>
2023-09-02 11:35:34 -0300

    Adicionado os outros sound ambientes e adicionado no arquivo js

--- src/FocusTimer/sounds.js
+++ src/FocusTimer/sounds.js
@@ -1,4 +1,8 @@
-export const buttonPressAudio = new Audio('./assets/button-press.wav')
-export const kitchenTimer = new Audio('./assets/kichen-timer.mp3')
-export const bgAudio = new Audio('./assets/bg-audio.mp3')
+export const buttonPressAudio = new Audio("./assets/button-press.wav");
+export const kitchenTimer = new Audio("./assets/kitchen-timer.mp3");
+export const bgAudio = new Audio("./assets/bg-audio.mp3");
 
+export const coffeeShop = new Audio("./assets/Cafeteria.wav");
+export const forest = new Audio("./assets/Floresta.wav");
+export const rain = new Audio("./assets/Chuva.wav");
+export const fireplace = new Audio("./assets/Lareira.wav");
